86b078ea2cd241fed1063df4df51ea552fe28773
NNish <>
2025-05-12 13:43:29 +0700

    ui

--- Assets/Scripts/TargetScoreZone.cs
+++ Assets/Scripts/TargetScoreZone.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class TargetScoreZone : MonoBehaviour
 {
 
- public AudioSource audioSource;
+    public AudioSource audioSource;
     private void OnCollisionEnter(Collision collision)
     {
 
